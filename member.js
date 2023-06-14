0a904e6a8b98c66a46a8a4fdda151c285a5d052e
wumingyu-2023 <135683636+wumingyu-2023@users.noreply.github.com>
2023-06-14 02:24:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillMember() {
+  var member = {
+    name: 'John Doe',
+    age: 20,
+    skills: ['JavaScript', 'CSS', 'HTML']
+  };
+  console.log(member.name); // John Doe
+  console.log(member.age); // 20
+  console.log(member.skills[0]); // JavaScript
+  console.log(member.skills[1]); // CSS
+  console.log(member.skills[2]); // HTML
+}
